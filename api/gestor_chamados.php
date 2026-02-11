55d5f5148110f2fd7180c54bd179f335356e3c50
Geovana Eduarda <geovana.e.oliveira3@aluno.senai.br>
2026-02-11 11:27:54 -0300

    Commit de atualizações de API -> 11/02/2026

--- api/gestor_chamados.php
+++ api/gestor_chamados.php
@@ -8,13 +8,7 @@ if (!isset($_SESSION['user_id']) || $_SESSION['user_perfil'] !== 'gestor') {
     exit;
 }
 
-$sql = "SELECT
-            chamados.id_chamado,
-            usuarios.nome,
-            ambientes.nome,
-            chamados.prioridade,
-            status
-        FROM chamados inner join usuarios on id_usuario = usuarios.id_usuario join ambientes on ambientes.id_ambiente = chamados.id_ambiente";
+$sql = "SELECT chamados.id_chamado, usuarios.nome as solicitante, ambientes.nome as local , chamados.prioridade, status FROM chamados inner join usuarios on chamados.id_solicitante = usuarios.id_usuario join ambientes on ambientes.id_ambiente = chamados.id_ambiente";
 
         $res = $conn->query($sql);
         $dados = $res->fetch_all(MYSQLI_ASSOC);
